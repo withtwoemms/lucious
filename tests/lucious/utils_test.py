cbf6e2d0d838b09f9d66207a7cdb4ab17cd22dd9
Emmanuel Obi <withtwoemms@gmail.com>
2022-02-23 20:17:02 -0600

    adds snowflake_cursor test

--- tests/lucious/utils_test.py
+++ tests/lucious/utils_test.py
@@ -1,5 +1,12 @@
+from os import environ as envvars
 from unittest import TestCase
+from unittest.mock import PropertyMock
+from unittest.mock import patch
+
+from snowflake.connector.cursor import SnowflakeCursor
+
 from lucious.utils import class_properties
+from lucious.utils import snowflake_cursor
 
 
 class UtilsTest(TestCase):
@@ -14,3 +21,20 @@ class UtilsTest(TestCase):
             dict(result),
             {'a': 'a', 'b': 'b'}
         )
+
+    @patch.dict(
+        envvars,
+        {
+            'SNOWFLAKE_ACCOUNT': 'snowflake',
+            'SNOWFLAKE_USER': 'user',
+            'SNOWFLAKE_PASSWORD': 'password',
+            'SNOWFLAKE_ROLE': 'role',
+            'SNOWFLAKE_WAREHOUSE': 'warehouse',
+        },
+        clear=True
+    )
+    @patch('snowflake.connector.connection.SnowflakeConnection.connect')
+    @patch('snowflake.connector.connection.SnowflakeConnection.rest', new_callable=PropertyMock)
+    def test_SnowflakeCursor(self, mock_property, mock_connect):
+        result = snowflake_cursor(database='database', schema='schema')
+        self.assertIsInstance(result, SnowflakeCursor)
